e3aa5b953abb88347a2df86299f64821eb4b0127
lfemia <laurabfemia@gmail.com>
2021-08-27 22:35:55 -0300

    primer desafio javascript

---
+++ claseuno+Femia/js/claseuno.js
@@ -0,0 +1,27 @@
+//Ejercicio 1
+let nombreUsuario;
+const lugarweb = 'mi primer desafio en Javascript';
+let numero1, numero2, resultado;
+let texto1;
+let texto2;
+
+nombreUsuario = prompt('Introduzca su nombre')
+alert('Hola ' + nombreUsuario + ', bienvenido a ' + lugarweb);
+
+//Ejercicio 2 
+alert('Introduzca su edad');
+numero1 = parseInt(prompt("Ingrese su edad:"));
+
+numero2 = 10;
+resultado = numero1 + numero2;
+alert(numero1 + " + " +  numero2 + " = " + resultado);
+alert('En 10 años, tendras ' + resultado);
+
+//Ejercicio3
+alert('¿De dónde sos?');
+
+texto1 = prompt('Mi país es');
+texto2 = prompt('La ciudad que vivo se llama')
+
+alert('Soy de ' + texto2 + ', ciudad ubicada en ' + texto1);
+console.log('Soy de ' + texto2 + ', ciudad ubicada en ' + texto1);
